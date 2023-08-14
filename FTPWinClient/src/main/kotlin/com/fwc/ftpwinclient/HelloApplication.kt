f6c555f5f896df2b922237dc5380f49c587efe26
Kalynovsky Valentin <nakama3942@gmail.com>
2023-08-14 22:45:11 +0300

    I did sketch of interface design

--- FTPWinClient/src/main/kotlin/com/fwc/ftpwinclient/HelloApplication.kt
+++ FTPWinClient/src/main/kotlin/com/fwc/ftpwinclient/HelloApplication.kt
@@ -8,9 +8,11 @@ import javafx.stage.Stage
 class HelloApplication : Application() {
     override fun start(stage: Stage) {
         val fxmlLoader = FXMLLoader(HelloApplication::class.java.getResource("hello-view.fxml"))
-        val scene = Scene(fxmlLoader.load(), 320.0, 240.0)
+        val scene = Scene(fxmlLoader.load(), 600.0, 800.0)
         stage.title = "Hello!"
         stage.scene = scene
+        stage.minWidth = 600.0
+        stage.minHeight = 800.0
         stage.show()
     }
 }
